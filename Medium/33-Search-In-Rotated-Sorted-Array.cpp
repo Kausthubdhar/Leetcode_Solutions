8b33abec9d251220a378e524805ad4e3607be606
Kode Kausthubdhar <132699709+Kausthubdhar@users.noreply.github.com>
2024-06-18 22:26:58 +0530

    solved 33

---
+++ Medium/33-Search-In-Rotated-Sorted-Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0, high = n-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[mid] <= nums[high]){
+                if(target < nums[mid] || target > nums[high]) high = mid - 1;
+                else low = mid + 1;
+            }
+            else{
+                if(target > nums[mid] || target < nums[low]) low = mid + 1;
+                else high = mid -1;;
+            }
+        }
+        return -1;
+    }
+};
