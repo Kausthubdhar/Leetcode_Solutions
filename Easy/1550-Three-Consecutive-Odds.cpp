9dc127e2361bdb955cdbbfd62caff50395de2ffe
Kode Kausthubdhar <132699709+Kausthubdhar@users.noreply.github.com>
2024-07-01 22:44:10 +0530

    solved 1550

---
+++ Easy/1550-Three-Consecutive-Odds.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int cnt = 0;
+        for(auto &it: arr){
+            if(it&1){
+                cnt++;
+                if(cnt >= 3) return true;
+            }
+            else cnt = 0;
+        }
+        return false;
+    }
+};
